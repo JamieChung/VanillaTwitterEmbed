cdccb8471401303c321f3939060877a45aba3799
Jamie Chung <me@jamiechung.me>
2011-12-15 12:10:23 -0500

    Able to get tweets to embed into posts.

--- class.vanillatwitterembed.plugin.php
+++ class.vanillatwitterembed.plugin.php
@@ -14,12 +14,45 @@ $PluginInfo['VanillaTwitterEmbed'] = array (
 	'AuthorUrl'			=>	'http://www.jamiechung.me'
 );
 
-class VanillaTwitterEmbedPlugin extends Gdn_Plugin 
+class VanillaTwitterEmbedPlugin implements Gdn_IPlugin 
 {
+	private $twitterStatusRegex = '/(http|https)\:\/\/twitter.com\/(.*?)\/status\/([0-9]+)/';
+	
 	public function Setup()
 	{
 		
 	}
+	
+	protected function CreateEmbed ( $matches )
+	{
+		$id = $matches[3];
+		$api = 'http://api.twitter.com/1/statuses/oembed.json?id='.$id;
+		$response = file_get_contents($api);
+		if ( !$response )
+		{
+			return false;
+		}
+		
+		$response = json_decode($response);
+		return $response->html;
+	}
+	
+	protected function TwitterEmbed ( $content )
+	{
+		$this->content = $content;
+		$content = preg_replace_callback($this->twitterStatusRegex, array($this, 'CreateEmbed'), $content);
+		
+		
+		return $content;
+	}
+	
+	    /**
+     * DiscussionController_BeforeCommentBody_Handler
+     * @param DiscussionController $Sender
+     */
+    public function DiscussionController_BeforeCommentBody_Handler(&$Sender) {
+        $Sender->EventArguments['Comment']->Body = $this->TwitterEmbed($Sender->EventArguments['Comment']->Body);
+    }
 	
 	public function Enabled ()
 	{
