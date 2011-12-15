b9f1fc96e94f8c1c87c1325ffd04416c2fd384fc
Jamie Chung <me@jamiechung.me>
2011-12-14 20:38:48 -0500

    Initial push to setup plugin.

---
+++ class.vanillatwitterembed.php
@@ -0,0 +1,15 @@
+<?php
+
+$PluginInfo['VanillaTwitterEmbed'] = array (
+	'Name'				=>	'Twitter Embed',
+	'Description'			=>	'Embed tweets directly into posts and messages.',
+	'Version'				=>	'0.1',
+	'RequiredApplications'	=>	array('Vanilla' => '2.0.18'),
+	'RequiredPlugins'		=>	FALSE,
+	'HasLocale'			=>	FALSE,
+	'SettingsUrl'			=>	'/dashboard/plugin/twitter-embed',
+	'SettingsPermission'	=>	'Garden.Settings.Manage',
+	'Author'				=>	'Jamie Chung',
+	'AuthorEmail'			=>	'me@jamiechung.me',
+	'AuthorUrl'			=>	'http://www.jamiechung.me'
+);
