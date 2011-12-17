eb65fde5f86f830aa25d5128b22078fd5c687eb1
JamieChung <me@jamiechung.me>
2011-12-17 15:57:20 -0500

    Finished plugin. Basic public release.

---
+++ README.md
@@ -0,0 +1,34 @@
+Vanilla Twitter Embed
+=======================================
+
+Latest Version: 0.1 (Dec.17.2011)
+-----------
+- Embed tweets directly into discussion posts by pasting the tweet URL.
+- Tweets are cached in database for faster performance.
+
+Installation
+-----------
+1. Simply drop the VanillaTwitterEmbed directory into your vanilla forum's plugin directory and enable in dashboard.
+
+2. Navigate to built in administration interface which can be found under Settings -> Vanilla Twitter Embed.
+
+Get Support and Make Contributions
+-----------
+- Fork a copy and make contributinos: https://github.com/JamieChung/VanillaTwitterEmbed
+- Get support on twitter: http://www.twitter.com/jamiechung
+
+ChangeLog
+-----------
+### 0.1 (Dec.14.2011)
+- Initial Public Release
+
+==============================
+
+Copyright 2011
+
+Jamie Chung
+
+me@jamiechung.me
+
+http://www.jamiechung.me
+
--- class.vanillatwitterembed.plugin.php
+++ class.vanillatwitterembed.plugin.php
@@ -1,14 +1,21 @@
 <?php
 
+/**
+* Vanilla Twitter Embed: Embed tweets directly into discussion posts by pasting the tweet URL.
+* @author Jamie Chung <me@jamiechung.me>
+* @homepage http://www.jamiechung.me
+* @twitter @jamiechung
+* @github https://github.com/JamieChung/VanillaTwitterEmbed
+*/
+
+
 $PluginInfo['VanillaTwitterEmbed'] = array (
 	'Name'				=>	'Twitter Embed',
-	'Description'			=>	'Embed tweets directly into posts and messages.',
+	'Description'			=>	'Embed tweets directly into discussion posts by pasting the tweet URL.',
 	'Version'				=>	'0.1',
 	'RequiredApplications'	=>	array('Vanilla' => '2.0.18'),
 	'RequiredPlugins'		=>	FALSE,
 	'HasLocale'			=>	FALSE,
-	'SettingsUrl'			=>	'/dashboard/plugin/twitter-embed',
-	'SettingsPermission'	=>	'Garden.Settings.Manage',
 	'Author'				=>	'Jamie Chung',
 	'AuthorEmail'			=>	'me@jamiechung.me',
 	'AuthorUrl'			=>	'http://www.jamiechung.me'
@@ -16,72 +23,105 @@ $PluginInfo['VanillaTwitterEmbed'] = array (
 
 class VanillaTwitterEmbedPlugin implements Gdn_IPlugin 
 {
+	// Regex for replace callback. Identifies a url to a individual tweet.
 	private $twitterStatusRegex = '/(http|https)\:\/\/twitter.com\/(.*?)\/status\/([0-9]+)/';
 	
+	/**
+	* Setup the database structure required to cache the used tweets.
+	*/
 	public function Setup()
 	{
-		
+		Gdn::Database()->Query("CREATE TABLE IF NOT EXISTS `GDN_TwitterEmbed` (
+							  `ID` int(11) NOT NULL AUTO_INCREMENT,
+							  `TweetID` varchar(255) NOT NULL,
+							  `Response` text NOT NULL,
+							  PRIMARY KEY (`ID`),
+							  KEY `TweetID` (`TweetID`)
+							)");
 	}
 	
+	/**
+	* If a match is found within the body of a post, the
+	* proper code is injected in place of it.
+	* @param $matches array Matched results from the twitter status regex.
+	*/
 	protected function CreateEmbed ( $matches )
 	{
+		// Tweet ID is the third matched result.
 		$id = $matches[3];
-		$api = 'http://api.twitter.com/1/statuses/oembed.json?id='.$id.'&omit_script=false';
-		$response = file_get_contents($api);
-		if ( !$response )
-		{
-			return false;
-		}
 		
-		$response = json_decode($response);
+		$tweet = false;
+		
+		// We cannot force $id to be an int datatype because the value is too large for PHP.
+		if ( ctype_digit($id) )
+		{
+			// Check if the selected tweet is in the database.
+			$tweet = Gdn::Database()->SQL()
+						->Select('*')
+						->From('TwitterEmbed')
+						->Where('TweetID = ', $id)
+						->Limit(1)
+						->Get()
+						->FirstRow();
+		
+		}
+				
+		// If we don't have the tweet, let's get it from the twitter server.
+		if ( !$tweet )
+		{
+			$api = 'http://api.twitter.com/1/statuses/oembed.json?id='.$id.'&omit_script=true';
+			$response = file_get_contents($api);
+			
+			// If we can't get the proper response from the server
+			// simply return the matched result (url link in post)
+			if ( !$response )
+			{
+				return $matches[0];
+			}
+			
+			// Insert the matched tweet and the cached results into the database.
+			Gdn::Database()->SQL()
+				->Insert('TwitterEmbed', array(
+					'TweetID' => $id,
+					'Response' => $response,
+				));
+								
+			$response = json_decode($response);
+		}
+		else
+		{
+			$response = json_decode($tweet->Response);	
+		}
+			
 		return $response->html;
 	}
 	
+	/**
+	* Setup the callback to replace the content of the post with the oembed code.
+	*/
 	protected function TwitterEmbed ( $content )
 	{
 		$this->content = $content;
 		$content = preg_replace_callback($this->twitterStatusRegex, array($this, 'CreateEmbed'), $content);
-		
-		
 		return $content;
 	}
 	
 	/**
-	* DiscussionController_BeforeCommentBody_Handler
-	* @param DiscussionController $Sender
+	* Setup the callback for every page that handles comments to a post.
 	*/
-	public function DiscussionController_BeforeCommentBody_Handler ( &$Sender )
+	public function Base_AfterCommentFormat_Handler ( &$Sender )
 	{
-		$Sender->EventArguments['Comment']->Body = $this->TwitterEmbed($Sender->EventArguments['Comment']->Body);
-	}
-	
-	public function Enabled ()
-	{
-		return ( C('Plugins.TwitterEmbed.Enabled') == TRUE );
-	}
-	
-	public function Controller_Toggle ( $Sender )
-	{
-		if ( Gdn::Session()->ValidateTransientKey(GetValue(1, $Sender->RequestArgs)) )
-		{
-			if ( C('Plugins.TwitterEmbed.Enabled') )
-			{
-				RemoveFromConfig('Plugins.TwitterEmbed.Enabled');
-				
-				$Sender->SQL("DROP TABLE IF EXISTS `gdn_tweetembed`");
-			}
-			else
-			{
-				SaveToConfig('Plugins.TwitterEmbed.Enabled', TRUE);
-				$Sender->SQL("CREATE TABLE IF NOT EXISTS `gdn_tweetembed` (
-							  `ID` int(11) NOT NULL AUTO_INCREMENT,
-							  `TweetID` int(11) NOT NULL,
-							  `CacheResponse` text NOT NULL,
-							  PRIMARY KEY (`ID`)
-							)");
-			}
-		}
+		$Object = $Sender->EventArguments['Object'];
 		
-		redirect('plugin/twitter-embed');
+		$Object->FormatBody = $this->TwitterEmbed($Object->Body);
+		$Sender->EventArguments['Object'] = $Object;
+	}
+	
+	/**
+	* Injects the twitter widget into all pages.
+	*/
+	public function Base_Render_Before ( $Sender )
+	{
+		$Sender->AddJsFile('https://platform.twitter.com/widgets.js');
 	}
 }
